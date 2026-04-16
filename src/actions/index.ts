202570d31b9ebfc688caacda949a2f01f113a3ed
Stephen Sorley <stephen.sorley@cccgainesville.org>
2026-04-16 04:02:52 -0400

    fixed bad glob path in form handler loader, added vite error message to detect missing form handlers

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,17 +2,24 @@ import { basename, extname } from "node:path";
 
 // Put actions in here.
 export const server: Record<string, any> = {
-  
+
 }
 
 
 // Automatically grab form handlers from each form, and add them to the server
 // instance. Uses the filename without extension, prefixed by the word "handle",
 // as the action name. E.g.: ContactForm.astro --> handleContactForm
-const formModules = import.meta.glob("./components/forms/*Form.astro", {
+const formModules = import.meta.glob("/src/components/forms/*Form.astro", {
   import: 'handleForm',
   eager: true,
 });
 for(const [path, func] of Object.entries(formModules)) {
-  server["handle"+basename(path, extname(path))] = func;
+  if (func) {
+    server["handle"+basename(path, extname(path))] = func;
+  } else {
+    console.error(
+`Can't find form handler in ${path}.
+Form handler should be defined in the frontmatter like this:
+ "export const handleForm = defineAction(...)"`);
+  }
 }
