c5ad3c1fa4c55a13291913b8d5422d49ebb6bc08
Stephen Sorley <stephen.sorley@cccgainesville.org>
2026-04-17 00:28:26 -0400

    forms: extract common zod stuff into its own file for reuse
    (this also keeps the individual forms looking cleaner)

---
+++ src/components/forms/zod-common.ts
@@ -0,0 +1,57 @@
+/**
+ * Zod schema fields and error messages that are used across multiple forms.
+ * 
+ * To use:
+ *   import { zc } from "./zod-common.ts"
+ *   ...
+ *   z.object {
+ *     phone: zc.us_phone,
+ *     email: zc.optional(zc.email),
+ *     ...
+ *   }
+ * 
+ * Notes on us_phone:
+ *   This will accept a number of different US phone number formats, it's fairly
+ *   lenient. Anything it accepts will be normalized to 555-555-5555, however.
+ * 
+ *   Examples of accepted inputs:
+ *    352-555-0132     <-- other variants will all be normalized to this one.
+ *    (352) 555-0132
+ *    352.555.0132
+ *    352 555 0132
+ *    3525550132
+ *    tel+13525550132  <-- format seen when copy/pasting telephone number links.
+ */
+import { z } from 'astro/zod';
+
+export const zc = {
+  full_name: z
+    .string("Enter your full name (John Smith).")
+    .max(255, "Enter a shorter name (255 letters max)."), // Salesforce length limit
+
+  email: z
+    .email("Enter a valid email (jsmith@example.com)")
+    .max(80, "Enter a shorter email (80 letters max)."), // Salesforce length limit
+
+  us_phone: z
+    .string("Enter a valid phone number (352-555-0132)")
+    .max(40) //for security - prevent malicious senders from hammering regex with very large strings
+    .regex(/^(tel:\+1)?[\(]*[0-9]{3}[ .\-\)]*[0-9]{3}[ .\-]*[0-9]{4}$/,
+      "Enter a valid phone number (352-555-0132)")
+    .transform((phone)=> {
+      // Remove all chars from phone number that aren't digits, then add dashes
+      // at the right spots: xxx-xxx-xxxx
+      const digits = phone.replace("tel:+1","").replace(/\D/g, "");
+      return `${digits.slice(0,3)}-${digits.slice(3,6)}-${digits.slice(6,10)}`;
+    }),
+
+
+  /*
+    Improved version of Zod's optional() that works better for forms by correctly
+    interpreting the empty string as the field being unset.
+  
+    Example usage: zc.optional(zc.us_phone)
+  */
+  optional: (schema: z.ZodType) => (
+    z.preprocess((v) => (v === "" ? undefined : v), schema.optional())),
+}
