345a2e100573ec938dd108ca7f8de1f1a0949fe0
Stephen Sorley <stephen.sorley@cccgainesville.org>
2026-04-25 19:49:33 -0400

    add explanatory comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,32 @@
 import { defineMiddleware } from 'astro:middleware'
 
-/* Unique ID generator function that works in both static and SSR modes.
+/* [onRequest] is executed after a page is requested, but before rendering
+ * begins. This happens for both static pages (pre-rendering) and dynamic pages
+ * (server-side rendering - SSR).
  *
- * This is useful for components that need to specify a unique ID internally
- * (like a disclosure and its trigger button), but might also be used multiple
- * times on the same page.
- * 
- * Use this by calling Astro.locals.id('<component name>') inside your component.
- * 
- * See: https://andrewmara.com/blog/generate-unique-ids-per-request-in-astro
+ * Values stored in [context.locals] are available to use inside the .astro files 
+ * that define the page, through the [Astro.locals] object.
  */
 export const onRequest = defineMiddleware(async (context, next) => {
+
+  /* Unique ID generator that works in both static and SSR modes.
+   *
+   * This is useful for components that need to specify a unique ID internally
+   * (like a disclosure and its trigger button), but might also be used multiple
+   * times on the same page.
+   * 
+   * Use this by calling Astro.locals.id('<component name>') inside your component.
+   * 
+   * See: https://andrewmara.com/blog/generate-unique-ids-per-request-in-astro
+   */
   context.locals.id = (() => {
-    // The map to store how many times each prefix has been used in this request.
-    const map = new Map<string, number>()
+    const map = new Map<string, number>(); // prefix -> number of times used so far
     return (prefix: string) => {
-      const count = (map.get(prefix) ?? 0) + 1
-      map.set(prefix, count)
+      const count = (map.get(prefix) ?? 0) + 1;
+      map.set(prefix, count);
       return (count === 1)? `${prefix}` : `${prefix}-${count}`;
     }
-  })()
+  })();
 
   return await next();
 })
