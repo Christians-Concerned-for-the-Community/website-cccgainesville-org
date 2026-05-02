f98345b78641237179dbd46cbffcb1b8d6049dcc
Stephen Sorley <stephen.sorley@cccgainesville.org>
2026-05-02 15:56:57 -0400

    minor comment fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-/* Type for Astro.locals.id function (defined in middelware.ts). */
+/* Type for custom middleware functions (defined in middleware.ts). */
 declare namespace App {
   interface Locals {
     id: (prefix: string) => string,
