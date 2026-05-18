3b719903c951535253538190f726096388117cd7
Stephen Sorley <stephen.sorley@cccgainesville.org>
2026-05-17 23:50:49 -0400

    remove leftover types from old middleware functions

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-/* Type for custom middleware functions (defined in middleware.ts). */
-declare namespace App {
-  interface Locals {
-    id: (prefix: string) => string,
-    first: (name: string) => boolean,
-  }
-}
