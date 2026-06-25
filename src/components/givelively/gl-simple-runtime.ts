0716660a751508dfdac4a5f723c151ceec79319a
Stephen Sorley <stephen.sorley@gmail.com>
2026-06-25 16:41:53 -0400

    updated givelively widget to use code from give-lively-simple project

--- src/components/givelively/gl-simple-runtime.ts
+++ src/components/givelively/gl-simple-runtime.ts
@@ -60,6 +60,7 @@
       }
 
       if (!supports_invoker && dialog?.id) {
+        // Manually add event listeners to open and close buttons, if Invoker API isn't supported.
         const invokers = document.querySelectorAll(`.gl-simple-donation-widget [commandfor="${dialog.id}"]`);
         for (const invoker of invokers) {
           const command = invoker.getAttribute("command");
@@ -74,7 +75,7 @@
         }
       }
     });
-  } // END polyfills and bugfixes for dialogs
+  } // END polyfills and bugfixes for dialogs -----
 
   const donateForm = qsroot("> form") as HTMLFormElement;
 
@@ -86,8 +87,8 @@
   const firstOnetime = qs(amountSet, ".gl-amt-value-onetime input") as HTMLInputElement | null;
   const firstRecurring = qs(amountSet, ".gl-amt-value-recurring input") as HTMLInputElement | null;
 
-  const donateSuffix = qs(donateForm, ".gl-donate-button span[aria-hidden]");
-  const donateSuffixAlt = qs(donateForm, ".gl-donate-button span.sr-only");
+  const donateSuffix = qs(donateForm, ".gl-donate-suffix");
+  const donateSuffixAlt = qs(donateForm, ".gl-donate-suffix-alt");
   
   const dedButton = qs(donateForm, ".gl-ded-button");
 
@@ -109,10 +110,10 @@
   const iframeUrl = new URL(`https://secure.givelively.org/donate/${donateForm.dataset.slug}`);
   const thisUrl = new URL(document.URL);
 
-  const locale = donateForm.dataset.locale;
+  const locale = donateForm.dataset.locale || "en-US";
   const settings: Intl.NumberFormatOptions = {
     style: "currency",
-    currency: donateForm.dataset.currency,
+    currency: donateForm.dataset.currency || "USD",
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   };
