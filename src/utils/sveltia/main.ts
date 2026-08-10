771647968f8fdd2ac6b5434b0e5715eb6e0fb2a7
Stephen Sorley <stephen.sorley@cccgainesville.org>
2026-08-10 19:49:22 -0400

    editor: add config for nav menu, move config out into separate files in utils/sveltia

---
+++ src/utils/sveltia/main.ts
@@ -0,0 +1,43 @@
+import { init, type CmsConfig } from '@sveltia/cms';
+
+import { siteConfig } from './site-config';
+
+export const setupSveltia = (githubToken: string | undefined) => {
+  // If we have a Github PAT available to use, store it in localStorage where
+  // Sveltia puts its session data. This lets us log in without user interaction,
+  // using the PAT.
+  if (githubToken) {
+    const userKey = "sveltia-cms.user";
+    const userVal = localStorage.getItem(userKey);
+
+    const user: Record<string,any> = userVal? JSON.parse(userVal) : {};
+    user.backendName = "github";
+    user.token = githubToken;
+
+    localStorage.setItem(userKey, JSON.stringify(user));
+  }
+
+  const config: CmsConfig = {
+    load_config_file: false,
+
+    app_title: "CCC Website Editor",
+    site_url: location.hostname,
+    logo: {
+      src: '/favicon.svg',
+      show_in_header: true,
+    },
+
+    backend: {
+      name: "github",
+      repo: "Christians-Concerned-for-the-Community/website-cccgainesville-org",
+    },
+
+    media_folder: "src/assets/media",
+
+    collections: [
+      siteConfig,
+    ]
+  };
+
+  init({config: config});
+};
---
+++ src/utils/sveltia/site-config.ts
@@ -0,0 +1,68 @@
+import type { CmsConfig } from '@sveltia/cms';
+type Collection = NonNullable<CmsConfig["collections"]>[number];
+
+export const siteConfig: Collection = {
+    label: "Site Configuration",
+    name: "config",
+    icon: "public",
+    editor: { preview: false },
+    files: [
+      {
+        label: "Navigation Menu",
+        name: "nav",
+        icon: "menu_open",
+        file: "src/assets/nav.json",
+        fields: [{
+          label: "Nav Menu Link",
+          name: "body",
+          widget: "list",
+          root: true,
+          collapsed: true,
+          min: 2,
+          max: 8,
+          fields: [
+            {
+              label: "Link Title",
+              name: "label",
+              widget: "string",
+              required: true,
+            },
+            {
+              label: "URL",
+              name: "href",
+              widget: "string",
+              default: "/",
+              required: true,
+              before_input: "https://cccgainesville.org",
+            },
+          ],
+        }],
+      },
+
+      {
+        label: "Headers",
+        name: "headers",
+        icon: "label_important",
+        file: "public/_headers",
+        format: "raw",
+        fields: [{
+          label: "Headers",
+          name: "body",
+          widget: "text",
+        }],
+      },
+
+      {
+        label: "Redirects",
+        name: "redirects",
+        icon: "airline_stops",
+        file: "public/_redirects",
+        format: "raw",
+        fields: [{
+          label: "Redirects",
+          name: "body",
+          widget: "text",
+        }],
+      },
+    ],
+  }
